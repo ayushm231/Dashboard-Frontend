0273b93a298c05ad9133cd88b3e99697bf0f619e
ayushm231 <63597900+ayushm231@users.noreply.github.com>
2024-04-23 17:30:03 +0530

    update : updated API_URL
    Old Value: API_URL - vercel-ten
    New Value: API_URL - vercel-delta

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:5001"
+// const API_URL = "http://localhost:5001"
 
-// const API_URL = "https://dashboard-backend-delta.vercel.app";
+const API_URL = "https://dashboard-backend-delta.vercel.app";
 
 export default API_URL;
